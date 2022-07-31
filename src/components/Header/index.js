14cfdc5b1a74bb36309fa0f92b13f1a50f50e565
Andrew Levin <anlevin@umich.edu>
2022-07-31 19:02:27 -0400

    routing and footer - after vid 2

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import './styles.scss'
+import { Link } from 'react-router-dom'
 
-import Logo from './../../assets/brl_logo.png'
+import Logo from './../../assets/logo.png'
 
 const Header = props => {
     return (
         <header className='header'>
             <div className="wrap">
                 <div className='logo'>
-                    <img src={Logo} alt="BRL Logo" />
+                    <Link to="/">
+                        <img src={Logo} alt="Simple Tut LOGO" />
+                    </Link>
+                </div>
+
+                <div className="callToActions">
+                    <ul>
+                        <li> 
+                        <Link to="/registration">
+                            Register
+                        </Link> 
+                        </li>
+                    </ul>
                 </div>
             </div>
-
         </header>
     );
 };
